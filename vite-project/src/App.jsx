bc89c9b56fb9c6170b7aa3af5821ef2a18a9032c
Anu9969 <sharmaanurag9969@gmail.com>
2025-01-03 01:01:58 +0530

    initialize Vite project with React setup and basic structure

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Component/>
+    <Component/>
+    <Component/>
+    </>
+  )
+}
+
+function Component(){
+  return(
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
