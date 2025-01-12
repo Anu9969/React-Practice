4f67a6de5fb46febf610e0c65f42bc2cbb63edab
Anu9969 <sharmaanurag9969@gmail.com>
2025-01-12 15:41:34 +0530

    add react-router-dom for navigation and implement basic routing in App component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,10 @@
 import { useState, useEffect } from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 
 
 function App() {
-  // const [count, setCount ] = useState(0);
-  // const [count2, setCount2] = useState(0);
 
-  const [currentTab, setCurrentTab] = useState(1);
-  const [tabData, setTabData] = useState({});
-  const [Loading, setLoading ] = useState(true);
     
   // function increment(){
   //   setCount(currentCount => currentCount + 1);
@@ -29,80 +25,44 @@ function App() {
 
   // }, [count, count2]);
 
-  useEffect(() =>{
-    setLoading(true);
-    fetch('https://jsonplaceholder.typicode.com/todos/' + currentTab)
-      .then(async res => {
-        const json = await res.json();
-        setTabData(json);
-        setLoading(false);
-      })
-  },[currentTab]);
+
 
   return (
-    
-    <div style={{backgroundColor: 'lightblue', height: '100vh' ,display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '130px'}}>
-     
-    <button onClick={() => {
-      setCurrentTab(1)
-    }} style={{color: currentTab == 1? "red" : "black", padding: '8px', borderRadius: "6px", gap:"2px"}}>Anurag</button>
-    <button onClick={function(){
-      setCurrentTab(2)
-    }} style={{color: currentTab == 2 ? "red" : "black", padding: '8px', borderRadius: "6px", gap:"2px"}}>Vinu</button>
-    <button onClick={() =>{
-      setCurrentTab(3)
-    }} style={{color: currentTab == 3 ? "red" : "black" ,padding: '8px', borderRadius: "6px", gap:"2px"}}>Vedant</button>
-    <button onClick={() => {
-      setCurrentTab(4)
-    }} style={{color: currentTab == 4 ? "red" : "black", padding: '8px', borderRadius: "6px", gap:"2px"}}>Anku</button>
-      
-      
-    {Loading ? "Loading..." : tabData.title}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/neet/online-coaching-class-11" element={<Class11program/>} />
+        <Route path = "/neet/online-coaching-class-12" element={<Class12program/>} />
+        <Route path = "/" element={<Landing/>} />
+      </Routes>
+    </BrowserRouter>
      
     
   )
 }
 
+function Class11program(){
+  return (
+    <div>
+      <h1>Class 11th</h1>
+    </div>
+  );
+}
 
+function Class12program(){
+  return (
+    <div>
+      <h1>Class 12th</h1>
+    </div>
+  );
+}
 
-
-
-
-
-
-// const ToggleMessage = () =>{
-//   // let [message, setMessage] = useState(false);
-//   let [notification, setNotification] = useState(0);
-
-//    function toggle(){
-//       // setMessage(!message);
-//       setNotification(notification + 1);
-//    }
- 
-  
-
-//   return(
-//     <div style={{borderRadius:'200px',width:'170px',border: '1px solid black', padding: '60px', margin: '10px',display: 'inline-block',backgroundColor: 'lightgreen',textAlign: 'center'}}>
-//     <button onClick={toggle} style={{padding: '10px',borderRadius:"4px"}}>Toggle Message</button>
-//     {/* {message && <h1>Message Visible yay i did it</h1>} */}
-
-//     {notification && <h1>Notification: {notification}</h1>}
-//     </div>
-//   );
-
-// }
-
-
-// function Component(){
-//   return(
-//     <div style={{display: 'inline-grid', justifyContent: 'space-between', alignItems: 'self-start',backgroundColor: 'lightblue'}}>
-//     <div style={{borderRadius:'200px',width:'170px',border: '1px solid black', padding: '60px', margin: '10px',backgroundColor: 'lightgreen'}}>
-//       <h1 style={{}}>Hello World</h1>
-//     </div>
-//     </div>
-//   )
-// }
+function Landing(){
+  return (
+    <div>
+      <h1>Landing Page</h1>
+    </div>
+  );
+}
 
 
 
